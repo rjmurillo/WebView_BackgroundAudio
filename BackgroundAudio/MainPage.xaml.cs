08697f4da0888117a8ebd7713d6a997ba1321da4
Josh Holmes <josh.holmes@microsoft.com>
2018-10-23 17:22:40 -0700

    updated for using addWebAllowedObject

--- BackgroundAudio/MainPage.xaml.cs
+++ BackgroundAudio/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BackgroundAudioWinRT;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -32,23 +33,54 @@ namespace BackgroundAudio
             this.InitializeComponent();
 
             this.Loaded += OnLoaded;
+
+            App.Current.EnteredBackground += Current_EnteredBackground;
+            App.Current.LeavingBackground += Current_LeavingBackground;
         }
 
+        private void Current_EnteredBackground(object sender, Windows.ApplicationModel.EnteredBackgroundEventArgs e)
+        {
+            if (wv1 != null)
+            {
+                wv1 = null;
+            }
+            if (wv2 != null)
+            {
+                wv2 = null;
+            }
+        }
+
+        private void Current_LeavingBackground(object sender, Windows.ApplicationModel.LeavingBackgroundEventArgs e)
+        {
+            Load();
+        }
+
+        private WebView wv1 = null;
+        private WebView wv2 = null;
         private void OnLoaded(object sender, RoutedEventArgs e)
         {
-            // Create web views
+            Load();
+        }
 
-            var wv1 = CreateWebView(0);
-            var wv2 = CreateWebView(1);
-
-            wv1.Source = Selection;
-            wv2.Source = Player;
+        private void Load()
+        {
+            if (wv1 == null)
+            {
+                wv1 = CreateWebView(0);
+                wv1.Source = Selection;
+            }
+            if (wv2 == null)
+            {
+                wv2 = CreateWebView(1);
+                wv2.Source = Player;
+            }
         }
 
         private WebView CreateWebView(int row)
         {
             var wv = new WebView(WebViewExecutionMode.SeparateProcess);
             wv.Settings.IsJavaScriptEnabled = true;
+            wv.AddWebAllowedObject("mediaPlayer", PlaybackService.Instance);
             WireUpWebViewDiagnostics(wv);
             Grid.SetRow(wv, row);
             this.Grid.Children.Add(wv);
@@ -73,21 +105,21 @@ namespace BackgroundAudio
         {
             if (sender is WebView wv)
             {
-                if (wv.Source.PathAndQuery.Equals(Selection.PathAndQuery, StringComparison.OrdinalIgnoreCase))
-                {
-                    Debug.WriteLine("Invoking script on " + wv.Source + ". Args: " + e.Value);
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                    Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                    {
-                        var wv2 = this.Grid.Children[1] as WebView;
-                        wv2?.InvokeScriptAsync("mediaPlayback", new[] { e.Value });
-                    });
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                } else if (wv.Source.PathAndQuery.Equals(Player.PathAndQuery, StringComparison.OrdinalIgnoreCase))
-                {
-                    var wv1 = this.Grid.Children[0] as WebView;
-                    wv1?.InvokeScriptAsync("loadState", new []{e.Value});
-                }
+////                if (wv.Source.PathAndQuery.Equals(Selection.PathAndQuery, StringComparison.OrdinalIgnoreCase))
+////                {
+////                    Debug.WriteLine("Invoking script on " + wv.Source + ". Args: " + e.Value);
+////#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+////                    Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+////                    {
+////                        var wv2 = this.Grid.Children[1] as WebView;
+////                        wv2?.InvokeScriptAsync("mediaPlayback", new[] { e.Value });
+////                    });
+////#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+////                } else if (wv.Source.PathAndQuery.Equals(Player.PathAndQuery, StringComparison.OrdinalIgnoreCase))
+////                {
+////                    var wv1 = this.Grid.Children[0] as WebView;
+////                    wv1?.InvokeScriptAsync("loadState", new []{e.Value});
+////                }
             }
 
 
---
+++ BackgroundAudioWinRT/PlaybackService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.Media.Core;
+using Windows.Media.Playback;
+
+namespace BackgroundAudioWinRT
+{
+    /// <summary>
+    /// The central authority on playback in the application
+    /// providing access to the player and active playlist.
+    /// </summary>
+    [AllowForWeb]
+    public sealed class PlaybackService
+    {
+        static PlaybackService instance;
+
+        public static PlaybackService Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new PlaybackService();
+
+                return instance;
+            }
+        }
+
+        public void Play()
+        {
+            this.Player.Play();
+        }
+        public void Pause()
+        {
+            this.Player.Pause();
+        }
+        public double Volume
+        {
+            get { return this.Player.Volume; }
+            set { this.Player.Volume = value; }
+        }
+        public void addItem(string uriString)
+        {
+            Uri s = new Uri(uriString);
+            if (this.Player.Source == null)
+                this.Player.Source = new MediaPlaybackList();
+
+
+            MediaPlaybackItem item = new MediaPlaybackItem(MediaSource.CreateFromUri(s));
+            (this.Player.Source as MediaPlaybackList).Items.Add(item);
+        }
+
+    /// <summary>
+    /// This application only requires a single shared MediaPlayer
+    /// that all pages have access to. The instance could have 
+    /// also been stored in Application.Resources or in an 
+    /// application defined data model.
+    /// </summary>
+    public MediaPlayer Player { get; private set; }
+
+        /// <summary>
+        /// The data model of the active playlist. An application might
+        /// have a database of items representing a user's media library,
+        /// but here we use a simple list loaded from a JSON asset.
+        /// </summary>
+//        public MediaList CurrentPlaylist { get; set; }
+
+        public PlaybackService()
+        {
+            // Create the player instance
+            Player = new MediaPlayer();
+            Player.AutoPlay = false;
+        }
+    }
+}
